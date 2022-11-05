2b22a45b5de2c06dcfc3eaac7b458a61a2b5b544
Aman Adatia <73164771+adatiaaman@users.noreply.github.com>
2022-11-05 15:22:30 +0530

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -6,7 +6,7 @@ uid INT NOT NULL,
 ac_count INT NOT NULL,
 sl_count INT NOT NULL,
 DOJ DATE NOT NULL,
-Primary key(uid)
+Primary key(uid, doj)
 );
 
 CREATE TABLE AC_CC(
@@ -21,13 +21,6 @@ Type VARCHAR(2) not null,
 Primary key(berth_no)
 );
 
-CREATE TABLE remaining_tickets(
-uid INT not null,
-ac_rem INT not null,
-sl_rem INT not null,
-DOJ date not null,
-Primary key(uid, DOJ)
-);
 
 CREATE TABLE ticket_records(
 uid INT not null,
@@ -37,11 +30,10 @@ DOJ date not null,
 Coach_no INT not null,
 Coach_type VARCHAR(2) not null, 
 Seat_type VARCHAR(2) not null,
-PNR INT not null,
+PNR VARCHAR (20) not null,
 passenger_name VARCHAR(20) not null,
 Primary key(uid, berth_no, DOJ, coach_no, coach_type, seat_type)
 );
-
 CREATE TABLE station(
 Uid INT not null,
 DOJ date not null,
@@ -50,3 +42,55 @@ Departure_time time not null,
 Order_of_station INT,
 Primary key (uid,DOJ,arrival_time)
 );
+
+-- dummy data query
+Insert into train(uid,ac_count,sl_count,DOJ) values(12891,  5, 6, ‘2022-12-10’);
+Insert into train(uid,ac_count,sl_count,DOJ) values(12891,  5, 6, ‘2022-12-11’);
+
+Insert into ac_cc(berth_no,type) values(1,'lb');
+Insert into ac_cc(berth_no,type) values(2,'lb');
+Insert into ac_cc(berth_no,type) values(3,'ub');
+Insert into ac_cc(berth_no,type) values(4,'ub');
+Insert into ac_cc(berth_no,type) values(5,'sl');
+Insert into ac_cc(berth_no,type) values(6,'su');
+Insert into ac_cc(berth_no,type) values(7,'lb');
+Insert into ac_cc(berth_no,type) values(8,'lb');
+Insert into ac_cc(berth_no,type) values(9,'ub');
+Insert into ac_cc(berth_no,type) values(10,'ub');
+Insert into ac_cc(berth_no,type) values(11,'sl');
+Insert into ac_cc(berth_no,type) values(12,'su');
+Insert into ac_cc(berth_no,type) values(13,'lb');
+Insert into ac_cc(berth_no,type) values(14,'lb');
+Insert into ac_cc(berth_no,type) values(15,'ub');
+Insert into ac_cc(berth_no,type) values(16,'ub');
+Insert into ac_cc(berth_no,type) values(17,'sl');
+Insert into ac_cc(berth_no,type) values(18,'su');
+
+
+
+
+
+Insert into sl_cc(berth_no,type) values(1,'lb');
+Insert into sl_cc(berth_no,type) values(2,'mb');
+Insert into sl_cc(berth_no,type) values(3,'ub');
+Insert into sl_cc(berth_no,type) values(4,'lb');
+Insert into sl_cc(berth_no,type) values(5,'mb');
+Insert into sl_cc(berth_no,type) values(6,'ub');
+Insert into sl_cc(berth_no,type) values(7,'sl');
+Insert into sl_cc(berth_no,type) values(8,'su');
+Insert into sl_cc(berth_no,type) values(9,'lb');
+Insert into sl_cc(berth_no,type) values(10,'mb');
+Insert into sl_cc(berth_no,type) values(11,'ub');
+Insert into sl_cc(berth_no,type) values(12,'lb');
+Insert into sl_cc(berth_no,type) values(13,'mb');
+Insert into sl_cc(berth_no,type) values(14,'ub');
+Insert into sl_cc(berth_no,type) values(15,'sl');
+Insert into sl_cc(berth_no,type) values(16,'su');
+Insert into sl_cc(berth_no,type) values(17,'lb');
+Insert into sl_cc(berth_no,type) values(18,'mb');
+Insert into sl_cc(berth_no,type) values(19,'ub');
+Insert into sl_cc(berth_no,type) values(20,'lb');
+Insert into sl_cc(berth_no,type) values(21,'mb');
+Insert into sl_cc(berth_no,type) values(22,'ub');
+Insert into sl_cc(berth_no,type) values(23,'sl');
+Insert into sl_cc(berth_no,type) values(24,'su');
